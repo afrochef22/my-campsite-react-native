a9aa0a265406d6f464c89259ae0693ba93319561
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2021-11-21 21:44:32 -0800

    Favorites

--- components/MainComponents.js
+++ components/MainComponents.js
@@ -5,6 +5,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoritesComponent";
 import {
 	View,
 	Platform,
@@ -165,6 +166,31 @@ const ReservationNavigator = createStackNavigator(
 	}
 );
 
+const FavoritesNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Favorites },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+			headerLeft: (
+				<Icon
+					name="heart"
+					type="font-awesome"
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView
@@ -211,6 +237,15 @@ const MainNavigator = createDrawerNavigator(
 			navigationOptions: {
 				drawerIcon: ({ tintColor }) => (
 					<Icon name="tree" type="font-awesome" size={24} color={tintColor} />
+				),
+			},
+		},
+		Favorites: {
+			screen: FavoritesNavigator,
+			drawerLabel: "My Favorites",
+			navigationOptions: {
+				drawerIcon: ({ tintColor }) => (
+					<Icon name="heart" type="font-awesome" size={24} color={tintColor} />
 				),
 			},
 		},
